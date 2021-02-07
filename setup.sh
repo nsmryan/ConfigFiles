c149add5ef83cbbb849a2018e9527a440605c830
Noah Ryan <nsmryan@gmail.com>
2021-02-07 10:49:03 -0500

    Fixed mkdir

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 
+mkdir -p ~/.vim/autoload/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.tmux/plugins
+mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 if [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
