10e6e587bc3da591716404bc6564a339b5e15230
Noah Ryan <nsmryan@gmail.com>
2021-02-07 10:53:43 -0500

    Attempting to make idempotent

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 
-mkdir -p ~/.vim/autoload/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    mkdir -p ~/.vim/autoload/
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    mkdir -p ~/.tmux/plugins
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 if [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
-    pacman -S tmux tcl
+    pacman -S tmux --noconfirm
 elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
     echo "Cygwin installation through setup.exe"
 fi
