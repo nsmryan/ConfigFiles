3ca94294dbffb2d9b6571697b5ec1388ad11363e
Noah Ryan <nsmryan@gmail.com>
2021-02-07 10:47:31 -0500

    Moving to using a setup.sh instead of vim_plug, which will do more setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+if [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
+    pacman -S tmux tcl
+elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
+    echo "Cygwin installation through setup.exe"
+fi
