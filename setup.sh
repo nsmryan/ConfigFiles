01df32d4dbc26fc11420ba18a68864ecce223733
Noah Ryan <nsmryan@gmail.com>
2021-02-07 17:36:37 -0500

    Added initial implementation of se tool

--- setup.sh
+++ setup.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# install vim plugin manager
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
     mkdir -p ~/.vim/autoload/
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+# install tmux plugin manager
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     mkdir -p ~/.tmux/plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
+# detect OS and perform OS-specific commands
 if [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
     pacman -S tmux --noconfirm
 elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
     echo "Cygwin installation through setup.exe"
 fi
 
+# use nice color scheme
 source base16-gruvbox-dark-soft.sh
+
+# put .vimrc in home
+cp ./.vimrc ~/
+
+# install se tool
+cp se /usr/bin/
+
+# install plugins in a separate process...
+vim -c ":PlugInstall" &
 
